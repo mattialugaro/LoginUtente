63a90d9a40c21b465ad907fdde8ca5d3b4e32a7c
mattia lugaro <mattialugaro@gmail.com>
2024-02-01 16:58:15 +0100

    Aggiungere i file di progetto.

---
+++ LoginUtente/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUtente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Continua = "";
+            do
+            {
+                Console.WriteLine("===============OPERAZIONI==============");
+                Console.WriteLine("Scegli l'operazione da effettuare:");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Logout");
+                Console.WriteLine("3. Verifica ora e data di login");
+                Console.WriteLine("4. Lista accessi");
+                Console.WriteLine("5. Esci");
+                int scelta = Convert.ToInt16(Console.ReadLine());
+
+                switch (scelta)
+                {
+                    case 1:
+                        Utente.Login();
+                        Console.WriteLine("Vuoi continuare? (y/n)");
+                        Continua = Console.ReadLine();
+                        break;
+                    case 2:
+                        Utente.Logout();
+                        Console.WriteLine("Vuoi continuare? (y/n)");
+                        Continua = Console.ReadLine();
+                        break;
+                    case 3:
+                        Utente.StampaAccesso();
+                        Console.WriteLine("Vuoi continuare? (y/n)");
+                        Continua = Console.ReadLine();
+                        break;
+                    case 4:
+                        Utente.StoricoUtenti();
+                        Console.WriteLine("Vuoi continuare? (y/n)");
+                        Continua = Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("Chiusura del programma in corso...");
+                        Continua = "n";
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non prevista...");
+                        Console.WriteLine("Vuoi continuare? (y/n)");
+                        Continua = Console.ReadLine();
+                        break;
+
+                }
+            }
+            while (Continua == "y");
+                Console.ReadLine();
+
+            
+        }
+        
+    }
+}
---
+++ LoginUtente/Utente.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LoginUtente
+{
+    public static class Utente
+    {
+        public static string username;
+        public static string password;
+        public static bool isLogged;
+        public static DateTime DataOraLog;
+        public static List<string> AccessiUtenti = new List<string>(); 
+
+        public static void Login()
+        {
+            Console.WriteLine("Inserire username:");
+            Utente.username = Console.ReadLine();
+            Console.WriteLine("Inserire password:");
+            Utente.password = Console.ReadLine();
+            Console.WriteLine("Inserire conferma password:");
+            string conferma = Console.ReadLine();
+
+            if (conferma == Utente.password && Utente.username != "")
+            {
+                Utente.isLogged = true;
+                Utente.DataOraLog = DateTime.Now;
+                Utente.AccessiUtenti.Add(Utente.username);
+                Console.WriteLine("Utente loggato correttamente");
+                
+            }
+            else
+            {
+                Console.WriteLine("Errore nella compilazione dei campi");
+            }
+
+        }
+
+        public static void Logout()
+        {
+            Utente.username = "";
+            Utente.password = "";
+            Utente.isLogged = false;
+            Console.WriteLine("Nessun utente loggato");
+        }
+        public static void StoricoUtenti()
+        {
+            foreach (var record in Utente.AccessiUtenti)
+            {
+                Console.WriteLine($" {record} \n ");
+            }
+        }
+
+        public static void StampaAccesso()
+        {
+            if(Utente.isLogged == true)
+            {
+                Console.WriteLine($"Orario accesso: {Utente.DataOraLog}");
+            }
+            else
+            {
+                Console.WriteLine("Necessario effettuare il login"); 
+            }
+        }
+
+
+    }
+
+    
+
+
+    
+}
